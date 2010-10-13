cf8e6a0eea5a700b0862d725d82968ca2b3b2b56
gino <gino.heyman@gmail.com>
2010-10-13 22:10:43 +0200

    VirtualDirectory -> virtualDirectory (element name)

--- lib/inetmgr/iis_object/application.rb
+++ lib/inetmgr/iis_object/application.rb
@@ -4,6 +4,6 @@ class Application  < IisObject
   prop :auto_start, "serviceAutoStartEnabled"
   prop :auto_start_provider, "serviceAutoStartProvider"
 
-  collection :virtual_directories, :VirtualDirectory, VirtualDirectory
+  collection :virtual_directories, :virtualDirectory, VirtualDirectory
  
 end
