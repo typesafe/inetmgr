ca19e1e289bdaac453c08bc9127e7ca34f4f54d8
gino <gino.heyman@gmail.com>
2010-10-14 20:58:02 +0200

    Added flag for rolling back when using configure in specs

--- spec/spec_env.rb
+++ spec/spec_env.rb
@@ -6,10 +6,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), "../lib/inetmgr"))
 
 module RSpecExtensions
 
-	def configure
+	def configure(apply_changes = true)
 		cfg = IisConfiguration.new
 		yield cfg
-	    cfg.apply_changes
+	    cfg.apply_changes if apply_changes
 	end
 
     def generate_random_name
